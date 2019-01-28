41c3633c8dddb417c2abd6ed262610625752fac4
Jayesh Iyer <iyer.j@husky.neu.edu>
2019-01-28 16:39:00 -0500

    adding comments in class

--- webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
+++ webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
@@ -4,9 +4,13 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class BCryptUtil {
 	
+	// Method to generate BCrypt encrypted password  
 	public String generateEncryptedPassword(String password) {
 		
+		// Generate a random salt of length 10
 		String salt = BCrypt.gensalt(10);
+		
+		// Generate encrypted password using salt and plaintext password
 		String encryptedPassword = BCrypt.hashpw(password, salt);
 		
 		return encryptedPassword;
