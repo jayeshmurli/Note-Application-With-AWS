8cfacdde64441560e3166fd649b55d4889330d5d
Jayesh Iyer <iyer.j@husky.neu.edu>
2019-01-27 19:38:45 -0500

    Adding method to generate encrypted password

--- webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
+++ webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
@@ -1,7 +1,15 @@
 package com.restapi.services;
 
-import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class BCryptUtil {
+	
+	public String generateEncryptedPassword(String password) {
+		
+		String salt = BCrypt.gensalt(10);
+		String encryptedPassword = BCrypt.hashpw(password, salt);
+		
+		return encryptedPassword;
+	}
 
 }
