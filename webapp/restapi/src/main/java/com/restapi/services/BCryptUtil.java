227bac3beb855831f67a93be9c8261b70662d80a
mitali-salvi <salvi.mi@northeastern.edu>
2019-01-27 19:37:15 -0500

    Added method for verifying password in BCryptUtil

--- webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
+++ webapp/restapi/src/main/java/com/restapi/services/BCryptUtil.java
@@ -2,6 +2,13 @@ package com.restapi.services;
 
 import org.springframework.security.crypto.bcrypt.*;
 
-public class BCryptUtil {
+public class BCryptUtil 
+{
+	public static boolean verifyPassword (String plainTextPassword, String storedHash)
+	{
+		boolean passwordVerified = false;
+		passwordVerified = BCrypt.checkpw(plainTextPassword, storedHash);
+		return passwordVerified;
+	}
 
 }
