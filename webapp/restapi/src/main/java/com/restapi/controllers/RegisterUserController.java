dc52c584d48de99c1305a36c9cb29c10014ffa41
Shubhankar Dandekar <shubhankard34@gmail.com>
2019-01-27 21:03:16 -0500

    Database integrated and register new user implemented

--- webapp/restapi/src/main/java/com/restapi/controllers/RegisterUserController.java
+++ webapp/restapi/src/main/java/com/restapi/controllers/RegisterUserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restapi.model.Credentials;
 import com.restapi.model.User;
 import com.restapi.services.RegisterService;
 
@@ -16,7 +17,7 @@ public class RegisterUserController {
 	private RegisterService registerService;
 
 	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
-	public User registerUser(@RequestBody User user) {
-		return this.registerService.registerUser(user);
+	public User registerUser(@RequestBody Credentials credentials) {
+		return this.registerService.registerUser(credentials);
 	}
 }
--- webapp/restapi/src/main/java/com/restapi/services/RegisterService.java
+++ webapp/restapi/src/main/java/com/restapi/services/RegisterService.java
@@ -1,14 +1,23 @@
 package com.restapi.services;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.restapi.model.Credentials;
 import com.restapi.model.User;
 
 @Service
 public class RegisterService {
-
+	@PersistenceContext
+	private EntityManager entityManager;
 	
-	public User registerUser(User user) {
+	@Transactional
+	public User registerUser(Credentials credentials) {
+		User user = new User(credentials.getUsername(), credentials.getPassword());
+		this.entityManager.persist(user);
 		return user;
 	}
 }
