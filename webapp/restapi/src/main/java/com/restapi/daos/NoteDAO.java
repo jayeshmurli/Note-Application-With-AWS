7df3caf3c0997b241c9933e72788349e235e2d00
mitali-salvi <salvi.mi@northeastern.edu>
2019-03-14 00:45:04 -0400

    Deleting note will delete attachment in NoteDAO

--- webapp/restapi/src/main/java/com/restapi/daos/NoteDAO.java
+++ webapp/restapi/src/main/java/com/restapi/daos/NoteDAO.java
@@ -1,21 +1,28 @@
 package com.restapi.daos;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.restapi.model.Attachment;
 import com.restapi.model.Note;
+import com.restapi.services.NoteService;
 
 @Service
 public class NoteDAO {
 
 	@PersistenceContext
 	private EntityManager entityManager;
+	
+	@Autowired
+	AttachmentDAO attachmentDAO;
 
 	public Note getNote(String id) {
 		TypedQuery<Note> query = this.entityManager.createQuery("SELECT n from Note n where n.id = ?1",
@@ -34,8 +41,19 @@ public class NoteDAO {
 	public void deleteNote(String id) 
 	{
 		Note noteToBeDeleted = this.entityManager.find(Note.class, id);
+		
+		System.out.println("Deleting notes attached to note");
+		List<Attachment> attachments = attachmentDAO.getAttachmentFromNote(noteToBeDeleted);
+		
+		for (Attachment attachment : attachments)
+		{
+			attachmentDAO.deleteAttachment(attachment.getId());
+		}
+		System.out.println("DONE deleting all attachments");
+		
+		System.out.println("Finally Deleting note");
 		this.entityManager.remove(noteToBeDeleted);
-		flushAndClear();		
+		flushAndClear();
 	}
 	
 	public Note getNoteFromId(String id) 
