72e788403f9dd65ce49a6c935a40be123e9719f9
Jayesh Iyer <iyer.j@husky.neu.edu>
2019-03-14 12:55:33 -0400

    Changing logic to take env variables in Spring properties

--- webapp/restapi/startApplication.sh
+++ webapp/restapi/startApplication.sh
@@ -1,8 +1,9 @@
-tail /etc/profile -n 5 > /tmp/tempFile.sh
-sudo chmod 777 /tmp/tempFile.sh
-sudo fuser -k 80/tcp
-echo "cd /var" >> /tmp/tempFile.sh
-echo "sudo java -jar restapi.jar --server.port=80 --spring.datasource.url=jdbc:mysql://\$DB_HOST:\$DB_PORT/csye6225 --spring.datasource.username=\$DB_USERNAME --spring.datasource.password=\$DB_PASSWORD --cloud.islocal=false --cloud.bucketName=\$S3_BUCKET" >> /tmp/tempFile.sh
-cd /tmp/
+#tail /etc/profile -n 5 > /tmp/tempFile.sh
+#sudo chmod 777 /tmp/tempFile.sh
+#sudo fuser -k 80/tcp
+#echo "cd /var" >> /tmp/tempFile.sh
+#echo "sudo java -jar restapi.jar --server.port=80 --spring.datasource.url=jdbc:mysql://\$DB_HOST:\$DB_PORT/csye6225 --spring.datasource.username=\$DB_USERNAME --spring.datasource.password=\$DB_PASSWORD --cloud.islocal=false --cloud.bucketName=\$S3_BUCKET" >> /tmp/tempFile.sh
+#cd /tmp/
 
-./tempFile.sh > run.out 2> run.err &
+#./tempFile.sh > run.out 2> run.err &
+sudo java -jar restapi.jar --server.port=80 -Dspring.profiles.active=jayesh-dev
