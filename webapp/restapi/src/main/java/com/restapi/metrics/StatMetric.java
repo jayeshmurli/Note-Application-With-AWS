150c152f2e4648e64399670f478be5fda5c5f5a8
Jayesh Iyer <iyer.j@husky.neu.edu>
2019-03-20 19:48:51 -0400

    Metric logger test run

--- webapp/restapi/src/main/java/com/restapi/metrics/StatMetric.java
+++ webapp/restapi/src/main/java/com/restapi/metrics/StatMetric.java
@@ -1,5 +1,7 @@
 package com.restapi.metrics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.timgroup.statsd.NonBlockingStatsDClient;
@@ -7,9 +9,13 @@ import com.timgroup.statsd.StatsDClient;
 
 @Service
 public class StatMetric {
+	
+	private static final Logger logger = LoggerFactory.getLogger(StatMetric.class);
+	
 	private static final StatsDClient statsd = new NonBlockingStatsDClient("csye6225.webapp.restapi", "localhost", 8125);
 	
 	public void increementStat(String endpoint) {
+		logger.info("Incrementing counter for : " + endpoint);
 		statsd.increment(endpoint);
 	}
 	
