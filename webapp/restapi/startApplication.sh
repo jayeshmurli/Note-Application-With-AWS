7b15b1c3704b4cc95525d7c8ad02bddb7d82c37c
Jayesh Iyer <iyer.j@husky.neu.edu>
2019-03-21 00:11:05 -0400

    Restarting cloudwatch agent on each EC2 deploy

--- webapp/restapi/startApplication.sh
+++ webapp/restapi/startApplication.sh
@@ -5,9 +5,8 @@ echo "source /etc/profile" >> /tmp/tempFile.sh
 sudo chmod 777 /tmp/tempFile.sh
 sudo fuser -k 80/tcp
 echo "cd /var" >> /tmp/tempFile.sh
+echo "sudo systemctl restart amazon-cloudwatch-agent" >> /tmp/tempFile.sh
 echo "sudo java -jar restapi.jar --server.port=80 --spring.datasource.url=jdbc:mysql://\$DB_HOST:\$DB_PORT/csye6225 --spring.datasource.username=\$DB_USERNAME --spring.datasource.password=\$DB_PASSWORD --cloud.islocal=false --cloud.bucketName=\$S3_BUCKET" >> /tmp/tempFile.sh
-echo "sudo systemctl stop amazon-cloudwatch-agent" >> /tmp/tempFile.sh
-echo "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s" >> /tmp/tempFile.sh
 
 cd /tmp/
 
