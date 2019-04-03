0c01d69bd20ad740b8ad2de64370e80225672db4
Shubhankar Dandekar <shubhankard34@gmail.com>
2019-04-03 16:26:52 -0400

    Load Testing Script

---
+++ jmeter/loadTest.sh
@@ -0,0 +1,19 @@
+echo "Enter Path to JMETER binaries:"
+read JMETER_BIN
+
+echo "Enter Path to Test Plan file (.jmx):"
+read TEST_PLAN_PATH
+
+echo "Enter Path to place the output log file:"
+read LOG_FILE_PATH
+
+echo "Enter IP of the web application server:"
+read SERVER_IP 
+
+echo "Enter Port of the web application server:"
+read SERVER_PORT 
+
+
+$JMETER_BIN/jmeter.sh -n -t $TEST_PLAN_PATH -l $LOG_FILE_PATH -JServerIP=$SERVER_IP -JServerPort=$SERVER_PORT
+
+
